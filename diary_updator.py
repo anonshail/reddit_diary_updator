399de3cf7e2ecdd8bac056ff4fafacb9027a1f0e
anonshail <anonshail@gmail.com>
2018-07-29 22:36:23 +0530

    submissions added

--- diary_updator.py
+++ diary_updator.py
@@ -1,4 +1,5 @@
 import praw
+import time
 
 def init_subreddit():
 	"this function is used to initialise a reddit instnace"
@@ -11,4 +12,15 @@ def init_subreddit():
 
 	return reddit
 
-reddit = init_subreddit()
+reddit = init_subreddit()		#obtaining a reddit object
+post_title = time.asctime()		#post title, which is set to the current date and time
+self_text = input("please enter the text you wish to submit:\n")
+
+sure = input("Submit? (y/n) ")
+
+if(sure == 'y' or sure == 'Y'):
+	reddit.subreddit('DiaryOfMyLife').submit(post_title, selftext = self_text)
+	print("Submission made succesfully!")
+
+else:
+	print("Exitting . . .")
